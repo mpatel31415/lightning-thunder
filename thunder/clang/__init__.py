7381e4e20417aec1b8984bfc4b044161fe169f47
apaz-cli <aarpazdera@gmail.com>
2024-03-24 16:43:46 -0400

    Added scalar warning.

--- thunder/clang/__init__.py
+++ thunder/clang/__init__.py
@@ -9,7 +9,9 @@ import operator
 from types import EllipsisType, NoneType
 import copy
 import time
+import warnings
 
+from thunder.core.baseutils import run_once
 from thunder.core.compile_data import using_symbolic_values
 from thunder.clang.langctx import register_method
 from thunder.core.langctxs import langctx, Languages
@@ -1160,6 +1162,13 @@ def compute_broadcast_shape(*_shapes):
     return tuple(common_shape)
 
 
+@run_once
+def mT_scalar_warning():
+    warnings.warn(
+        "Tensor.mT is deprecated on 0-D tensors. This function is the identity in these cases.",
+        UserWarning,
+    )
+
 @clangop(method_name="mT")
 def matrix_transpose(a: TensorProxy) -> TensorProxy:
     """Transposes the last two dimensions of a tensor.
@@ -1184,6 +1193,7 @@ def matrix_transpose(a: TensorProxy) -> TensorProxy:
 
 
     if a.ndim == 0:
+        mT_scalar_warning()
         return a
     elif a.ndim == 1:
         raise RuntimeError(f"tensor.mT is only supported on matrices or batches of matrices. Got 1-D tensor.")
