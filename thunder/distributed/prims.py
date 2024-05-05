132b2ce7e55571fa3ffd6cdbb8b47a47013e1d53
Masaki Kozuki <mkozuki@nvidia.com>
2024-05-06 01:33:15 +0900

    return incoming grad as is if no_sync from synchronize backward for ddp
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- thunder/distributed/prims.py
+++ thunder/distributed/prims.py
@@ -10,6 +10,7 @@ from thunder.core.prims import make_prim
 
 from thunder.core.proxies import DDPType, FutureTensorProxy, pytype, TensorProxy
 from thunder.core.transforms import register_augmented_forward, register_backward
+from thunder.distributed import get_skip_data_parallel_grad_sync
 
 if TYPE_CHECKING:
     from thunder.common import CompileData
@@ -314,6 +315,8 @@ def synchronize_augmented_forward_rule(
 def synchronize_backward_rule(
     ddp_type: DDPType, group: torch.distributed.ProcessGroup, grad: TensorProxy
 ) -> tuple[TensorProxy, None]:
+    if get_skip_data_parallel_grad_sync() and ddp_type == DDPType.REPLICATED:
+        return grad, None
     preaverage_grad = grad / group.size()
     match ddp_type:
         case DDPType.REPLICATED:
